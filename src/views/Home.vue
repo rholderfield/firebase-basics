8c3f1cb736e8d9cb9f744a50cde7cff9d9acc350
rholderfield <raholderfield@gmail.com>
2022-04-02 15:23:21 -0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {firebaseConfig } from "../store/firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
